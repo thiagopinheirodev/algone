7f1330ad5138d75ca91b08cb312bcb6524f5ac15
thiagopinheirodev <thiagopinheiro.developer@gmail.com>
2023-01-05 08:36:58 -0300

    Data types

---
+++ types.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+	char a;
+	signed char b;
+	unsigned char c;
+	int d;
+	signed int e;
+	unsigned int f;
+	short int g;
+	signed short int h;
+	unsigned short int i;
+	long int j;
+	unsigned long int k;
+	signed long int l;
+	float m;
+	double n;
+	long double o;
+	
+	return 0;
+}
